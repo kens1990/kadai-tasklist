8eadbf96ecb9c5328b1e120eb0a84c7dac77dbb1
kens1990 <ken42574@yahoo.co.jp>
2018-04-03 16:13:46 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+     @task = Task.find(params[:id])
+  end
+  
+  def new
+      @task = Task.new
+  end
+  
+  def create
+     @task = Task.new(tasks_params)
+   if @task.save
+       flash[:success] = 'task  が正常に登録されました'
+       redirect_to @task
+   else
+      flash.now[:danger] = 'task が登録されませんでした'
+      render :new
+    end  
+  end
+  
+  def edit
+    @task = Task.find(params[:id])  
+  end
+  
+  def update
+      @task = Task.find(params[:id])
+      
+      if @task.update(task_params)
+      flash[:success] = 'task は正常に更新されました'
+      redirect_to @task
+      else
+      flash.now[:danger] = 'Message は正常に更新されませんでした'
+      render :edit 
+      end
+  end
+  
+  def destroy
+      
+      
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+end
