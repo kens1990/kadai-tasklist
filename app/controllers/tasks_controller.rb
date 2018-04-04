fc68f5de909b880f6bf9f5eac93ed7a208716169
kens1990 <ken42574@yahoo.co.jp>
2018-04-04 15:10:17 +0000

    tasklist +d

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
   
   def create
-     @task = Task.new(tasks_params)
+     @task = Task.new(task_params)
    if @task.save
        flash[:success] = 'task  が正常に登録されました'
        redirect_to @task
@@ -39,7 +39,11 @@ class TasksController < ApplicationController
   end
   
   def destroy
-      
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
       
   end
   
